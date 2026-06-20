66c8cc8be51bad6727ff8e5c0e32afa9fc8696a1
Eena Nuesca <geenanuesca@gmail.com>
2026-06-20 10:04:31 +0800

    Update: User Dashboard

--- frontend/src/com/visa/app/VisaUI.java
+++ frontend/src/com/visa/app/VisaUI.java
@@ -2143,7 +2143,7 @@ class ApplicantDashboardPanel extends JPanel {
         tableTitle.setForeground(Theme.TEXT_DARK);
         tableContainer.add(tableTitle, BorderLayout.NORTH);
 
-        String[] columns = { "ID", "Full Name", "Citizenship", "Submission Date/Birth Date", "Status" };
+        String[] columns = { "ID", "Full Name", "Citizenship", "Submission Date", "Status" };
         tableModel = new DefaultTableModel(columns, 0) {
             @Override
             public boolean isCellEditable(int row, int column) {
